5636a3fd5b2b83b6752b62d9a44206602bf0efa4
Bezerra <log.rotas.app@gmail.com>
2026-06-08 01:43:02 -0300

    Refactor Firebase initialization to destructure environment variables for improved readability and maintainability.

--- src/firebase.js
+++ src/firebase.js
@@ -2,16 +2,25 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-};
+const {
+  VITE_FIREBASE_API_KEY: apiKey,
+  VITE_FIREBASE_AUTH_DOMAIN: authDomain,
+  VITE_FIREBASE_PROJECT_ID: projectId,
+  VITE_FIREBASE_STORAGE_BUCKET: storageBucket,
+  VITE_FIREBASE_MESSAGING_SENDER_ID: messagingSenderId,
+  VITE_FIREBASE_APP_ID: appId,
+  VITE_FIREBASE_MEASUREMENT_ID: measurementId,
+} = import.meta.env;
 
-const app = initializeApp(firebaseConfig);
+const app = initializeApp({
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId,
+});
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
